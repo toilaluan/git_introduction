031716e0694f5c0f1ae336b49f49dcc6e38dc674
toilaluan <toilaluan@github.com>
2023-03-01 16:07:56 +0700

    cal.py add call_circle_area feature~

--- cal.py
+++ cal.py
@@ -4,20 +4,24 @@ def cal_rectangle_perimeter(a, b):
     return 2*(a + b)
 
 def cal_circle_area(r):
-    return math.pi * pow(r, 2)
+    return 2*math.pi * pow(r, 2)
 
 def cal_rectangle_area(a, b):
     return a * b
 
 if __name__ == '__main__':
     f = int(input("choose function: \n\
-        0. cal_rectangle_perimeter \n \nPlease enter an integer:"))
+        0. cal_rectangle_perimeter \n\
+        1. cal_circle_area \n\
+                   \nPlease enter an integer:"))
 
     if f == 0:
         a = int(input("Input value a: "))
         b = int(input("Input value b: "))
         result = cal_rectangle_perimeter(a, b)
-
+    elif f == 1:
+        r = int(input("Input value r: "))
+        result = cal_circle_area(r)
     else:
         result = "Wrong input"
     print(f"\nResult: {result}")
